6f396efe1ec15dd4ed414f7595e953f516cdff54
huang.xinghui <huang.x.hui@gmail.com>
2015-06-03 14:31:29 +0800

    change dropdown define

--- src/components/dropdown.js
+++ src/components/dropdown.js
@@ -3,118 +3,64 @@
  */
 
 var $ = require('jquery');
+var _ = require('underscore');
 var plugin = require('../plugin');
-var toggle   = '[data-toggle="dropdown"]';
-var backdrop = '.dropdown-backdrop';
+var toggle = '[data-toggle="dropdown"]';
 
-var Dropdown = plugin('dropdown', {
-    events: {
-        'click': 'toggle'
+plugin('dropdown', {
+    options: {
+        triggerEvent: 'click',
+        target: null
+    },
+
+    _create: function () {
+        this.delegate(this.options.triggerEvent, null, _.bind(this.toggle, this));
+
+        this.$parent = this.getParent();
+    },
+
+    open: function () {
+        if (this.isOpen()) return;
+
+        this._opened = true;
+        this.$parent.addClass('open');
+        this.trigger('open');
+    },
+
+    close: function () {
+        if (!this.isOpen()) return;
+
+        this._opened = false;
+        this.$parent.removeClass('open');
+        this.trigger('close');
     },
 
     toggle: function (e) {
-        var $this = $(this)
-    
-        if ($this.is('.disabled, :disabled')) return
-    
-        var $parent  = getParent($this)
-        var isActive = $parent.hasClass('open')
-    
-        clearMenus()
-
-        if (!isActive) {
-            if ('ontouchstart' in document.documentElement && !$parent.closest('.navbar-nav').length) {
-                // if mobile we use a backdrop because click events don't delegate
-                $('<div class="dropdown-backdrop"/>').insertAfter($(this)).on('click', clearMenus)
-            }
-    
-            var relatedTarget = { relatedTarget: this }
-            $parent.trigger(e = $.Event('show.bs.dropdown', relatedTarget))
-    
-            if (e.isDefaultPrevented()) return
-    
-            $this
-                .trigger('focus')
-                .attr('aria-expanded', 'true')
-    
-            $parent
-                .toggleClass('open')
-                .trigger('shown.bs.dropdown', relatedTarget)
-        }
-    
-        return false
+        this.isOpen() ? this.close() : this.open();
+        return false;
     },
-    
-    keydown: function (e) {
-        if (!/(38|40|27|32)/.test(e.which) || /input|textarea/i.test(e.target.tagName)) return
-    
-        var $this = $(this)
-    
-        e.preventDefault()
-        e.stopPropagation()
-    
-        if ($this.is('.disabled, :disabled')) return
-    
-        var $parent  = getParent($this)
-        var isActive = $parent.hasClass('open')
-    
-        if ((!isActive && e.which != 27) || (isActive && e.which == 27)) {
-            if (e.which == 27) $parent.find(toggle).trigger('focus')
-            return $this.trigger('click')
-        }
-    
-        var desc = ' li:not(.disabled):visible a'
-        var $items = $parent.find('[role="menu"]' + desc + ', [role="listbox"]' + desc)
-    
-        if (!$items.length) return
-    
-        var index = $items.index(e.target)
-    
-        if (e.which == 38 && index > 0)                 index--                        // up
-        if (e.which == 40 && index < $items.length - 1) index++                        // down
-        if (!~index)                                      index = 0
-    
-        $items.eq(index).trigger('focus')
-    }
 
+    isOpen: function () {
+        return this._opened;
+    },
+
+    getParent: function () {
+        if (this.options.target) {
+            return (this.options.target instanceof $ ? this.options.target : $(this.options.target)).parent();
+        } else {
+            return this.$element.parent();
+        }
+    }
 });
 
 function clearMenus(e) {
-    if (e && e.which === 3) return
-    $(backdrop).remove()
-    $(toggle).each(function () {
-        var $this         = $(this)
-        var $parent       = getParent($this)
-        var relatedTarget = { relatedTarget: this }
+    if (e && e.which === 3) return;
 
-        if (!$parent.hasClass('open')) return
-
-        $parent.trigger(e = $.Event('hide.bs.dropdown', relatedTarget))
-
-        if (e.isDefaultPrevented()) return
-
-        $this.attr('aria-expanded', 'false')
-        $parent.removeClass('open').trigger('hidden.bs.dropdown', relatedTarget)
-    })
-}
-
-function getParent($this) {
-    var selector = $this.attr('data-target')
-
-    if (!selector) {
-        selector = $this.attr('href')
-        selector = selector && /#[A-Za-z]/.test(selector) && selector.replace(/.*(?=#[^\s]*$)/, '') // strip for ie7
-    }
-
-    var $parent = selector && $(selector)
-
-    return $parent && $parent.length ? $parent : $this.parent()
+    $(toggle).dropdown('close');
 }
 
 $(document)
-    .on('click.bs.dropdown.data-api', clearMenus)
-    .on('click.bs.dropdown.data-api', '.dropdown form', function (e) { e.stopPropagation() })
-    .on('click.bs.dropdown.data-api', toggle, Dropdown.prototype.toggle)
-    .on('keydown.bs.dropdown.data-api', toggle, Dropdown.prototype.keydown)
-    .on('keydown.bs.dropdown.data-api', '[role="menu"]', Dropdown.prototype.keydown)
-    .on('keydown.bs.dropdown.data-api', '[role="listbox"]', Dropdown.prototype.keydown)
+    .on('click.dropdown.data-api', clearMenus)
+    .on('click.dropdown.data-api', '[data-toggle="dropdown"]', function (e) {
+        $(this).dropdown('toggle');
+    });
