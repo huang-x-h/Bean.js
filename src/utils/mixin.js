058dc3f79e876ff93e740e6eb868eff250916041
huang.xinghui <huang.x.hui@gmail.com>
2015-06-25 10:57:58 +0800

    add mixin util

---
+++ src/utils/mixin.js
@@ -0,0 +1,29 @@
+/**
+ * Created by huangxinghui on 2015/6/24.
+ */
+
+var _ = require('underscore');
+
+var Mixin = function (clazz, from) {
+    var to = clazz.prototype;
+
+    _.defaults(to, from);
+    _.defaults(to.options, from.options);
+
+    extendMethod(to, from, '_create');
+};
+
+function extendMethod(to, from, methodName) {
+    if (!_.isUndefined(from[methodName])) {
+        var old = to[methodName];
+
+        to[methodName] = function () {
+            var oldReturn = old.apply(this, arguments);
+            from[methodName].apply(this, arguments);
+            return oldReturn;
+        };
+
+    }
+}
+
+module.exports = Mixin;
