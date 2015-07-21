fcf3546b4d971d89aa79ddc1feaf94cd79aeb46c
huang.xinghui <huang.x.hui@gmail.com>
2015-07-21 22:10:34 +0800

    add progressbar component

---
+++ src/components/progressbar/index.js
@@ -0,0 +1,38 @@
+/**
+ * Created by huangxinghui on 2015/7/21.
+ */
+
+var $ = require('jquery');
+var Widget = require('../../widget');
+var plugin = require('../../plugin');
+var template = require('./progressbar.hbs');
+
+var ProgressBar = Widget.extend({
+    options: {
+        animation: false,
+        minimum: 0,
+        maximum: 100,
+        value: 0,
+        showLabel: false,
+        progressBarClass: ''
+    },
+
+    _create: function () {
+        this.$element.append(template(this.options));
+        this.$progressBar = this.$element.find('.progress-bar');
+        this.progress(this.options.value / this.options.maximum);
+    },
+
+    progress: function (value) {
+        var pct = (value * 100) + '%';
+        this.$progressBar.css('width', pct);
+
+        if (this.options.showLabel) {
+            this.$progressBar.text(pct);
+        }
+    }
+});
+
+plugin('progressbar', ProgressBar);
+
+module.exports = ProgressBar;
