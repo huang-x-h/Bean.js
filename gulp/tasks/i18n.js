137c0fe188513ffa60553186ef940ebad2f89072
huang.xinghui <huang.x.hui@gmail.com>
2015-08-15 22:31:08 +0800

    add i18n task

---
+++ gulp/tasks/i18n.js
@@ -0,0 +1,10 @@
+/**
+ * Created by huangxinghui on 2015/8/15.
+ */
+
+var gulp = require('gulp');
+
+gulp.task('i18n', function () {
+    return gulp.src('./src/locales/*')
+        .pipe(gulp.dest('./build/i18n/'));
+});
