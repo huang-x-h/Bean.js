26b161a1e6278dabde3d8abd122a817ff864368a
huang.xinghui <huang.x.hui@gmail.com>
2015-08-17 10:22:32 +0800

    $body set value when document ready

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -4,7 +4,10 @@
 
 var $document = require('./document');
 var locale = require('./locale');
+var $body = require('./body');
 
 $document.ready(function init() {
     locale.publish();
+
+    $body = $document.find('body');
 });
