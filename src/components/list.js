a49c9614cfcd8470dcb99ff624850140e631680f
huang.xinghui <huang.x.hui@gmail.com>
2015-09-11 15:26:31 +0800

    change list component

--- src/components/list.js
+++ src/components/list.js
@@ -10,26 +10,20 @@ var mixin = require('../utils/mixin');
 var ListMixin = require('../mixins/list');
 var Widget = require('../widget');
 
-function itemRendererHandler (data) {
-    var content = '';
-    if (_.isString(data)) {
-        content = data;
-    } else {
-        content = data[this.options.dataTextField];
-    }
-
-    return '<a href="javascript:;">' + content + '</a>';
+function defaultItemRenderer (data) {
+    return '<a href="javascript:;">' + this.itemToLabel(data) + '</a>';
 }
 
 var List = Widget.extend({
     options: {
         dataTextField: 'text',
         dataValueField: 'value',
-        itemRenderer: itemRendererHandler
+        itemRenderer: defaultItemRenderer
     },
 
     events: {
-        'click li': '_onClick'
+        'click li': '_onClick',
+        'keydown': '_onKeyDown'
     },
 
     _create: function () {
@@ -43,7 +37,7 @@ var List = Widget.extend({
         this._selectedItem = null;
 
         this.$element.html(this._dataSource.reduce(function (result, item) {
-            return result + '<li>' + _.bind(this.options.itemRenderer, this, item)() + '</li>';
+            return result + '<li>' + _.bind(this.options.itemRenderer, this, item, this.options.highlight)() + '</li>';
         }, '', this));
     },
 
@@ -66,6 +60,57 @@ var List = Widget.extend({
         }
 
         this.trigger('click');
+    },
+
+    _onKeyDown: function (e) {
+        var c = e.keyCode;
+
+        if (c === 38 || c === 40) { // Down/Up arrow
+            e.preventDefault();
+            this[c === 38 ? "previous" : "next"]();
+        }
+    },
+
+    previous: function () {
+        this.goto(this._selectedIndex - 1);
+    },
+
+    next: function () {
+        this.goto(this._selectedIndex + 1);
+    },
+
+    goto: function (index) {
+        this._setSelectedIndex(index);
+    },
+
+    itemToLabel: function (data) {
+        var label = '';
+        if (_.isString(data)) {
+            label = data;
+        } else if (_.isObject(data)) {
+            label = data[this.options.dataTextField];
+        }
+
+        return label;
+    },
+
+    itemToValue: function (data) {
+        var label = '';
+        if (_.isString(data)) {
+            label = data;
+        } else if (_.isObject(data)) {
+            label = data[this.options.dataValueField];
+        }
+
+        return label;
+    },
+
+    text: function () {
+        return this.itemToLabel(this._selectedItem);
+    },
+
+    value: function () {
+        return this.itemToValue(this._selectedItem);
     },
 
     _setSelection: function () {
