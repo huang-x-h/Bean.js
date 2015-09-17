7eeee1234b949bc361da36ec32f41bc53ad3b182
huang.xinghui <huang.x.hui@gmail.com>
2015-09-17 14:35:27 +0800

    change substitute method

--- src/utils/string.js
+++ src/utils/string.js
@@ -8,16 +8,11 @@ module.exports = {
     substitute: function (str) {
         if (str == null) return '';
 
-        var rest = Array.prototype.slice(arguments, 1);
-        var len = rest.length,
-            args, i;
+        var args = Array.prototype.slice(arguments, 1);
 
-        if (len == 1 && _.isArray(rest[0])) {
-            args = rest[0];
-            len = args.length;
+        if (args.length == 1 && _.isArray(rest[0])) {
+            args = args[0];
         }
-        else
-            args = rest;
 
         str = str.replace(/\{(\d+)\}/g, function (m, i) {
             return args[i]
