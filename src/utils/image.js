414de670e43f9cead69f95d76483777a5a8d8cd0
huang.xinghui <huang.x.hui@gmail.com>
2015-10-26 17:13:58 +0800

    add scroll image visible to load

--- src/utils/image.js
+++ src/utils/image.js
@@ -5,6 +5,8 @@
 var $ = require('jquery');
 var _ = require('underscore');
 var transition = require('../transition');
+var util = require('../utils/element');
+var Debouncer = require('../debouncer');
 
 function lazyLoad(element) {
   var $element = $(element),
@@ -14,7 +16,7 @@ function lazyLoad(element) {
   imageLoader.onload = function() {
     $element.addClass('fade');
     $element.attr('src', imageSource);
-    $element.addClass('in').one(transition.eventType, function() {
+    $element.addClass('in').one(transition.TRANSITION_END, function() {
       $element.removeAttr('data-lazy');
       $element.removeClass('fade in');
     }).emulateTransitionEnd();
@@ -22,18 +24,20 @@ function lazyLoad(element) {
 
   imageLoader.src = imageSource;
 }
-module.exports = Bean.image = {
-  lazyLoad: function(selector) {
-    if (selector instanceof $) {
-      selector.each(function(element) {
-        lazyLoad(element);
-      });
-    } else if (_.isString(selector)) {
-      $(selector).each(function(element) {
-        lazyLoad(element);
-      });
-    } else {
-      lazyLoad(selector);
+
+function render() {
+  $('img[data-lazy]').each(function(index, element) {
+    if (util.inViewport(element)) {
+      lazyLoad(element);
     }
+  })
+}
+
+module.exports = Bean.image = {
+  lazyLoad: function(options) {
+    var debouncer = new Debouncer(render);
+    window.addEventListener('scroll', debouncer, false);
+
+    render();
   }
 };
