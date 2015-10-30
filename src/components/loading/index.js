9ff30c875f1ead17680757025637f5b037a300ff
huang.xinghui <huang.x.hui@gmail.com>
2015-10-30 14:14:47 +0800

    revert loading template

--- src/components/loading/index.js
+++ src/components/loading/index.js
@@ -5,17 +5,11 @@
 var $ = require('jquery');
 var plugin = require('../../plugin');
 var Widget = require('../../widget');
-var template = '<div class="loading-screen">'
-    + '<div class="loading-center-outer">'
-    + '<div class="loading-center-middle">'
-    + '<img src="images/spin.gif">'
-    + '</div>'
-    + '</div>'
-    + '</div>';
+var template = require('./loading.hbs');
 
 var Loading = Widget.extend({
   _create: function() {
-    this.$loading = $(template);
+    this.$loading = $(template());
     this.$element.append(this.$loading);
   },
 
