baa9584f16dd9a3b488982e4463cef42c1f722cf
huang.xinghui <huang.x.hui@gmail.com>
2016-01-11 16:56:21 +0800

    add report for frontend report log or error to server

---
+++ src/utils/report.js
@@ -0,0 +1,24 @@
+/**
+ * Created by huangxinghui on 2016/1/11.
+ */
+
+var $ = require('jquery');
+
+function Report(prefix, url) {
+  this.prefix = prefix;
+  this.url = url;
+}
+
+Report.prototype.send = function(params) {
+  var image = new Image(),
+      imageID = this.prefix + Date.now();
+
+  image.onload = function() {
+    delete window[imageID];
+  };
+
+  window[imageID] = image;
+  image.src = this.url + '?' + $.param(params);
+};
+
+module.exports = Report;
