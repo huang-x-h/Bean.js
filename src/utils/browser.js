ec2dd8a1d1faa26142ccb3f10ec22f9c6a393891
huang.xinghui <huang.x.hui@gmail.com>
2016-01-12 15:17:09 +0800

    add useragent detect

---
+++ src/utils/browser.js
@@ -0,0 +1,25 @@
+/**
+ * Created by huangxinghui on 2016/1/12.
+ */
+
+// refer to https://github.com/ded/bowser/blob/master/bowser.js
+function detect(ua) {
+  var result;
+
+  function getFirstMatch(regex) {
+    var match = ua.match(regex);
+    return (match && match.length > 1 && match[1]) || '';
+  }
+
+  if (/msie|trident/i.test(ua)) {
+    result = {
+      name: 'Internet Explorer', msie: true, version: getFirstMatch(/(?:msie |rv:)(\d+(\.\d+)?)/i)
+    }
+  }
+
+  return result;
+}
+
+var browser = detect(navigator.userAgent);
+
+module.exports = browser;
